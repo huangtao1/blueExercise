90d067b3cd3979d1651e71e370c5465ed777b0af
huangtao <huangtao@oraro.net>
2020-04-28 15:41:48 +0800

    day4
    Change-Id: I725725a407bbae469c03d941a1c19500c601afa4

--- home_application/models.py
+++ home_application/models.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
-# from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class ScriptTemplates(models.Model):
+    name = models.CharField(max_length=50, verbose_name="脚本名称")
+    content = models.TextField(verbose_name="脚本内容")
+
+    def __repr__(self):
+        return "script name:%s" % self.name
--- home_application/views.py
+++ home_application/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .models import ScriptTemplates
 
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
@@ -36,4 +37,6 @@ def task_execute(request):
     """
     执行任务
     """
-    return render(request, 'home_application/task_execute.html')
+    tasks = ScriptTemplates.objects.all()
+    data = {"tasks": tasks}
+    return render(request, 'home_application/task_execute.html', data)
